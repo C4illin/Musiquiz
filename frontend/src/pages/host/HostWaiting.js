606f497827e8d7b5fb3ea6fe1bfed6288511e417
C4illin <emrik.ostling@gmail.com>
2022-10-24 23:46:57 +0200

    removed extra song

--- frontend/src/pages/host/HostWaiting.js
+++ frontend/src/pages/host/HostWaiting.js
@@ -3,7 +3,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import QR from '../../components/QR';
 import HostScreenStyles from '../../components/styles/HostScreenStyles';
-import SpotifyPlayer from '../../playback';
 
 const HostWaiting = ({ name }) => (
   <HostScreenStyles>
@@ -16,5 +15,4 @@ const HostWaiting = ({ name }) => (
 HostWaiting.propTypes = {
   name: PropTypes.string.isRequired,
 };
-SpotifyPlayer.controls.play(["spotify:track:4mn9xkejyNn8EBKhrOf3aW"], SpotifyPlayer.device_id);
 export default HostWaiting;
